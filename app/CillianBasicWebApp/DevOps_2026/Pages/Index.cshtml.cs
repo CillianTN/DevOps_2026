3beaa8e4ba71c2a0df5d2ded77d583b3a33ef411
Cillian Nolan <cillian.nolan@liebherr.com>
2026-03-05 17:24:02 +0000

    Added existing ASP.NET app

---
+++ app/CillianBasicWebApp/DevOps_2026/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevOps_2026.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
