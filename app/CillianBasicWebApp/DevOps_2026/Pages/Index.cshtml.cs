80f9cc496102b694e355f741a104f328cf805b1e
Cillian Nolan <cillian.nolan@liebherr.com>
2026-03-06 09:06:17 +0000

    Added a greeting message env from ConfigMap

--- app/CillianBasicWebApp/DevOps_2026/Pages/Index.cshtml.cs
+++ app/CillianBasicWebApp/DevOps_2026/Pages/Index.cshtml.cs
@@ -5,9 +5,11 @@ namespace DevOps_2026.Pages
 {
     public class IndexModel : PageModel
     {
+        public string Greeting { get; set; }
+
         public void OnGet()
         {
-
+            Greeting = Environment.GetEnvironmentVariable("APP_GREETING") ?? "Hello World (fallback)";
         }
     }
 }
